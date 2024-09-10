f37b0ce085b4b95d2b43ab383e0b2f3f3f807b05
82104 <82104@DESKTOP-CMBCKCL>
2024-09-10 20:17:57 +0900

    안녕

--- src/main/java/com/example/ourapp/JieunsehunApplication.java
+++ src/main/java/com/example/ourapp/JieunsehunApplication.java
@@ -9,6 +9,7 @@ public class JieunsehunApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JieunsehunApplication.class, args);
 		System.out.println("초지ㅣㅇ느 개나나바빠");
+		System.out.println("집가고 싶다");
 	}
 
 }
