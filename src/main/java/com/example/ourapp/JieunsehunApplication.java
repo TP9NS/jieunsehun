78f79eebcfb1afe4d05c72d66698af1918612f36
TP9NS <ajtwlstpgns@naver.com>
2024-09-10 19:47:07 +0900

    first commit

---
+++ src/main/java/com/example/ourapp/JieunsehunApplication.java
@@ -0,0 +1,13 @@
+package com.example.ourapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JieunsehunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JieunsehunApplication.class, args);
+	}
+
+}
