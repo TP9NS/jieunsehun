2f81ac31cc3d6fa11c5bc322580ff44d0f6acc30
TP9NS <ajtwlstpgns@naver.com>
2024-09-23 21:50:26 +0900

    메인 컨트롤러 추가

---
+++ src/main/java/com/example/ourapp/main/MainController.java
@@ -0,0 +1,26 @@
+package com.example.ourapp.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.ourapp.DTO.UserDTO;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Controller
+
+public class MainController {
+
+
+
+    @GetMapping("/login")
+    public String main() {
+        return "main.html";
+    }
+}
